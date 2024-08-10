1c281872fbb1ed6941df180684c603607f311c8e
jhezjkp <jhezjkp@163.com>
2024-08-10 21:37:36 +0800

    日志输出代码文件名及行号

--- utility/logger/logger.go
+++ utility/logger/logger.go
@@ -2,17 +2,27 @@ package logger
 
 import (
 	"context"
+	"fmt"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gctx"
 	"github.com/gogf/gf/v2/os/grpool"
+	"path"
+	"runtime"
 )
 
+func getCallerInfo() string {
+	_, file, line, ok := runtime.Caller(2)
+	if !ok {
+		return "unknown:0"
+	}
+	return fmt.Sprintf("%s:%d", path.Base(file), line)
+}
+
 func Debug(ctx context.Context, v ...interface{}) {
-	_ = grpool.AddWithRecover(gctx.NeverDone(ctx), func(ctx context.Context) { g.Log().Debug(ctx, v...) }, nil)
+	g.Log().Debug(ctx, v...)
 }
 
 func Info(ctx context.Context, v ...interface{}) {
-	//_ = grpool.AddWithRecover(gctx.NeverDone(ctx), func(ctx context.Context) { g.Log().Info(ctx, v...) }, nil)
 	g.Log().Info(ctx, v...)
 }
 
@@ -21,13 +31,16 @@ func Error(ctx context.Context, v ...interface{}) {
 }
 
 func Debugf(ctx context.Context, format string, v ...interface{}) {
-	_ = grpool.AddWithRecover(gctx.NeverDone(ctx), func(ctx context.Context) { g.Log().Debugf(ctx, format, v...) }, nil)
+	message := append([]interface{}{getCallerInfo()}, v...)
+	_ = grpool.AddWithRecover(gctx.NeverDone(ctx), func(ctx context.Context) { g.Log().Debugf(ctx, format, message...) }, nil)
 }
 
 func Infof(ctx context.Context, format string, v ...interface{}) {
-	_ = grpool.AddWithRecover(gctx.NeverDone(ctx), func(ctx context.Context) { g.Log().Infof(ctx, format, v...) }, nil)
+	message := append([]interface{}{getCallerInfo()}, v...)
+	_ = grpool.AddWithRecover(gctx.NeverDone(ctx), func(ctx context.Context) { g.Log().Infof(ctx, format, message...) }, nil)
 }
 
 func Errorf(ctx context.Context, format string, v ...interface{}) {
-	g.Log().Errorf(ctx, format, v...)
+	message := append([]interface{}{getCallerInfo()}, v...)
+	g.Log().Errorf(ctx, format, message...)
 }
