acd2f3714230e60d949565725c124b91e5a95faa
jhezjkp <jhezjkp@163.com>
2024-08-19 23:04:57 +0800

    更新构建脚本

--- build.sh
+++ build.sh
@@ -8,5 +8,11 @@ fi
 
 # build new fastapi
 echo "building fastapi..."
-GOOS=linux GOARCH=amd64 go build -o fastapi main.go
+
+BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
+GIT_COMMIT="$(git rev-parse HEAD)"
+VERSION="$(git describe --tags --abbrev=0 | tr -d '\n')"
+
+GOOS=linux GOARCH=amd64 go build -o fastapi -ldflags="-X 'github.com/iimeta/fastapi/internal/version.buildDate=${BUILD_DATE}' -X 'github.com/iimeta/fastapi/internal/version.gitCommit=${GIT_COMMIT}' -X 'github.com/iimeta/fastapi/internal/version.gitVersion=${VERSION}'" main.go
+
 echo "fastapi build complete!"
