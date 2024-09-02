e18e2049fcbbe1c550486a0cdded0e71d3ec94c8
jhezjkp <jhezjkp@163.com>
2024-09-03 00:03:01 +0800

    ansible自动部署

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 # check if fastapi file exists, the delete it
-if [ -f "fastapi" ]; then
-  echo "fastapi exists, deleting..."
-  rm fastapi
+target_file="deploy/fastapi"
+if [ -f "${target_file}" ]; then
+  echo "${target_file} exists, deleting..."
+  rm ${target_file}
 fi
 
 # build new fastapi
@@ -13,6 +14,8 @@ BUILD_DATE="$(date -u +'%Y-%m-%dT%H:%M:%SZ')"
 GIT_COMMIT="$(git rev-parse HEAD)"
 VERSION="$(git describe --tags --abbrev=0 | tr -d '\n')"
 
-GOOS=linux GOARCH=amd64 go build -o fastapi -ldflags="-X 'github.com/iimeta/fastapi/internal/version.buildDate=${BUILD_DATE}' -X 'github.com/iimeta/fastapi/internal/version.gitCommit=${GIT_COMMIT}' -X 'github.com/iimeta/fastapi/internal/version.gitVersion=${VERSION}'" main.go
+GOOS=linux GOARCH=amd64 go build -o ${target_file} -ldflags="-X 'github.com/iimeta/fastapi/internal/version.buildDate=${BUILD_DATE}' -X 'github.com/iimeta/fastapi/internal/version.gitCommit=${GIT_COMMIT}' -X 'github.com/iimeta/fastapi/internal/version.gitVersion=${VERSION}'" main.go
+
+chmod +x ${target_file}
 
 echo "fastapi build complete!"
