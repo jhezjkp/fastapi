228bd3bf9a5ff2e45261a691054a1b7a86e54346
jhezjkp <jhezjkp@163.com>
2024-11-11 18:24:13 +0800

    修正合并错误

--- internal/logic/common/client.go
+++ internal/logic/common/client.go
@@ -9,15 +9,15 @@ import (
 	"github.com/iimeta/fastapi/utility/logger"
 )
 
-func NewClient(ctx context.Context, model *model.Model, key, baseURL, path string) (sdk.Client, error) {
+func NewClient(ctx context.Context, corp string, model *model.Model, key, baseURL, path string) (sdk.Client, error) {
 
 	oss := config.Cfg.Oss
 	if model.IsEnablePresetConfig {
-		return sdk.NewClient(ctx, GetCorpCode(ctx, model.Corp), model.Model, key, baseURL, path, &model.PresetConfig.IsSupportSystemRole,
+		return sdk.NewClient(ctx, GetCorpCode(ctx, corp), model.Model, key, baseURL, path, &model.PresetConfig.IsSupportSystemRole,
 			oss.Endpoint, oss.Region, oss.AccessKey, oss.SecretKey, oss.Bucket, oss.Domain, config.Cfg.Http.ProxyUrl), nil
 	}
 
-	return sdk.NewClient(ctx, GetCorpCode(ctx, model.Corp), model.Model, key, baseURL, path, nil,
+	return sdk.NewClient(ctx, GetCorpCode(ctx, corp), model.Model, key, baseURL, path, nil,
 		oss.Endpoint, oss.Region, oss.AccessKey, oss.SecretKey, oss.Bucket, oss.Domain, config.Cfg.Http.ProxyUrl), nil
 }
 
